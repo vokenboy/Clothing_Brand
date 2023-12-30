b756d4be491a17ed4eba4624a14109192a94e60e
Vilius Tamašauskas <viliustam36@gmail.com>
2023-12-30 04:07:00 +0200

    Tailwind js
    Tailwindjs

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        'lg': '886px',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
